53184a5718c042df639a125541be3cd61749be08
notGonim <mhmodgonim96@gmail.com>
2021-01-05 07:57:16 +0200

    Intialize : components inside fb clone

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
+import { HomeComponent } from './../../../../angularPractice/src/app/home/home.component';
+import { FacebookGuard } from './guards/facebook.guard';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+
+    canActivate: [FacebookGuard],
+  },
+  { path: 'login', component: LoginComponent },
+  { path: 'signUp', component: RegisterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
